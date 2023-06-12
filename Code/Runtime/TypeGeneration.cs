a2d9acf362f413a12cd39516f0c38a5483271e83
Ramon Brullo <kingvanti650@gmail.com>
2023-06-12 14:53:48 +0200

    feat: correct implementation type name

--- Code/Runtime/TypeGeneration.cs
+++ Code/Runtime/TypeGeneration.cs
@@ -1,19 +1,44 @@
 ﻿#nullable enable
 
 using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace Dev.ComradeVanti.GameObjectAspect
 {
     internal static class TypeGeneration
     {
-        public static Type? TryGenerateImplementationType<T>()
+        private const TypeAttributes ImplementationTypeAttributes =
+            TypeAttributes.Public | TypeAttributes.Class;
+
+        private static bool IsConventionalInterfaceName(string interfaceName) =>
+            interfaceName.StartsWith("I") &&
+            interfaceName.Length >= 2 &&
+            char.IsUpper(interfaceName[1]);
+
+        private static string TypeNameFor(Type interfaceType)
+        {
+            var interfaceName = interfaceType.Name;
+            return IsConventionalInterfaceName(interfaceName)
+                ? interfaceName[1..] + "Implementation" // Skip the leading I
+                : interfaceName + "Implementation";
+        }
+
+        public static Type? TryGenerateImplementationType<T>(ModuleBuilder moduleBuilder)
             where T : class, IGameObjectAspect
         {
-            var aspectType = typeof(T);
+            var interfaceType = typeof(T);
 
-            if (!aspectType.IsInterface) return null;
+            if (!interfaceType.IsInterface) return null;
 
-            return aspectType;
+            var typeName = TypeNameFor(interfaceType);
+            var typeBuilder = moduleBuilder.DefineType(
+                typeName, ImplementationTypeAttributes,
+                typeof(object), new[] {interfaceType});
+
+
+            return typeBuilder.CreateType();
         }
     }
 }
--- Code/Tests/TypeGenerationTests.cs
+++ Code/Tests/TypeGenerationTests.cs
@@ -1,30 +1,75 @@
-﻿using NUnit.Framework;
+﻿#nullable enable
+
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using JetBrains.Annotations;
+using NUnit.Framework;
 
 namespace Dev.ComradeVanti.GameObjectAspect
 {
     public class TypeGenerationTests
     {
-        private abstract class ClassAspect : IGameObjectAspect
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+        public abstract class ClassAspect : IGameObjectAspect
         {
         }
 
-        private interface ITestAspect : IGameObjectAspect
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+        public interface IEmptyAspect : IGameObjectAspect
+        {
         {
         }
 
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+        // ReSharper disable once InconsistentNaming
+        public interface UnconventionalAspect : IGameObjectAspect
+        {
+        }
+
+
+        private static Type? TryGenerate<T>() where T : class, IGameObjectAspect
+        {
+            var testAssemblyBuilder =
+                AssemblyBuilder.DefineDynamicAssembly(
+                    new AssemblyName("TestsAssembly"), AssemblyBuilderAccess.Run);
+
+            var testModuleBuilder =
+                testAssemblyBuilder.DefineDynamicModule("TestModule");
+
+            return TypeGeneration.TryGenerateImplementationType<T>(testModuleBuilder);
+        }
 
         [Test]
         public void Aspects_Must_Not_Be_Classes()
         {
-            var maybeType = TypeGeneration.TryGenerateImplementationType<ClassAspect>();
+            var maybeType = TryGenerate<ClassAspect>();
             Assert.Null(maybeType);
         }
 
         [Test]
         public void Aspects_May_Be_Interfaces()
         {
-            var maybeType = TypeGeneration.TryGenerateImplementationType<ITestAspect>();
+            var maybeType = TryGenerate<IEmptyAspect>();
             Assert.NotNull(maybeType);
+        }
+
+        [Test]
+        public void Conventional_Interface_Names_Get_Correct_Implementation_Name()
+        {
+            // NOTE: The pattern is the interface name without the leading I and "Implementation" appended
+
+            var type = TryGenerate<IEmptyAspect>()!;
+            Assert.AreEqual(type.Name, "EmptyAspectImplementation");
+        }
+
+        [Test]
+        public void Unconventional_Interface_Names_Get_Correct_Implementation_Name()
+        {
+            // NOTE: The pattern is the interface name with "Implementation" appended
+
+            var type = TryGenerate<UnconventionalAspect>()!;
+            Assert.AreEqual(type.Name, "UnconventionalAspectImplementation");
         }
     }
 }
